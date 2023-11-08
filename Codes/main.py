283bc207796093de37926deb0f9f7106c747eb2c
Divya Sharma <divya.sharma@duke.edu>
2023-11-08 22:27:27 +0000

    updated codes

--- Codes/main.py
+++ Codes/main.py
@@ -1,6 +1,26 @@
-def main():
-    pass
+import time
+import argparse
+
+
+def factorial(n):
+    return 1 if n == 0 else n * factorial(n - 1)
+
+
+def benchmark(n):
+    start_time = time.time()
+    result = factorial(n)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    return result, elapsed_time
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("n", help="the number to compute the factorial of", type=int)
+    args = parser.parse_args()
+    result, elapsed_time = benchmark(args.n)
+    print("Python factorial:", result)
+    print("Time taken: {} seconds".format(elapsed_time))
+    with open("../Resources/Summary.md", "a") as f:
+        f.write("Python factorial: {}\n".format(result))
+        f.write("Python Time taken: {} seconds\n".format(elapsed_time))
--- Codes/src/main.rs
+++ Codes/src/main.rs
@@ -1,26 +1,27 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 use std::time::Instant;
 
 fn factorial(n: u128) -> u128 {
-    (1..=n).product()
+    match n {
+        0 | 1 => 1,
+        _ => n * factorial(n - 1),
+    }
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    let args: Vec<String> = env::args().collect();
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
     let num: u128 = args[1].parse().expect("Please provide a valid number");
     
     let start = Instant::now();
     let result = factorial(num);
     let duration = start.elapsed();
+    let time_taken = duration.as_secs_f64();
     
-    println!("Factorial of {} is {}", num, result);
-    println!("Time taken: {:?}", duration);
+    let mut file = File::create("../../Resources/Summary.md").expect("Unable to create file");
+    write!(file, "Rust factorial: {}\n", result).expect("Unable to write to file");
+    write!(file, "Rust Time taken for factorial: {} seconds\n", time_taken).expect("Unable to write to file");
     
-    c.bench_function("Rust Factorial", |b| {
-        b.iter(|| factorial(black_box(num)))
-    });
+    println!("Rust factorial: {}", result);
+    println!("Rust Time taken for factorial: {} seconds", time_taken);
 }
-
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
